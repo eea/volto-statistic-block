df367cbf7912d99ae7fbd0c5f20f1c9f590d1512
Miu Razvan <miu.razvan28@gmail.com>
2022-03-10 17:20:03 +0200

    add statistic block

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import installStatisticBlock from './StatisticBlock';
+
 const applyConfig = (config) => {
-  return config;
+  return [installStatisticBlock].reduce((acc, apply) => apply(acc), config);
 };
 
 export default applyConfig;
