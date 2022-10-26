d8d9e29abf91375357c63148b4405539978223bf
Alin Voinea <contact@avoinea.com>
2022-10-26 11:47:18 +0300

    refactor(volto-slate): Upgrade to @plone/volto-slate - refs #153447

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 import { isArray } from 'lodash';
-import { serializeNodes } from 'volto-slate/editor/render';
+import { serializeNodes } from '@plone/volto-slate/editor/render';
 
 export const serializeText = (text) => {
   return isArray(text) ? serializeNodes(text) : text;
