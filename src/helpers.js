ebbf6f7e10540a409626a19d7eb6b20e286c3b07
Miu Razvan <miu.razvan28@gmail.com>
2023-05-22 19:31:47 +0300

    fix: serialize nodes as it should be done

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,27 @@
-import { isArray } from 'lodash';
-import { serializeNodes } from '@plone/volto-slate/editor/render';
+import isArray from 'lodash/isArray';
+import isString from 'lodash/isString';
+import { Node } from 'slate';
+import config from '@plone/volto/registry';
+import { serializeNodes as _serializeNodes } from '@plone/volto-slate/editor/render';
 
-export const serializeText = (text) => {
-  return isArray(text) ? serializeNodes(text) : text;
-};
+function isValidNodesArray(value) {
+  return isArray(value) && Node.isNodeList(value);
+}
+
+export function createParagraph(text) {
+  return {
+    type: config.settings.slate.defaultBlockType,
+    children: [{ text }],
+  };
+}
+
+export function serializeToNodes(text) {
+  if (isValidNodesArray(text)) return text;
+  if (isString(text)) return [createParagraph(text)];
+  return [createParagraph('')];
+}
+
+export function serializeNodes(value) {
+  const nodes = serializeToNodes(value);
+  return _serializeNodes(nodes);
+}
