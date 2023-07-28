31d3b8084aa50e7d98fb1050cac706408b248a99
Miu Razvan <miu.razvan28@gmail.com>
2023-07-28 11:55:51 +0300

    chore: remove override of RichTextWidget

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import installStatisticBlock from './StatisticBlock';
-import RichTextWidget from './RichTextWidget';
 
 const applyConfig = (config) => {
-  config.widgets.widget.slate = RichTextWidget;
-  config.widgets.widget.slate_richtext = RichTextWidget;
-
   return [installStatisticBlock].reduce((acc, apply) => apply(acc), config);
 };
 
